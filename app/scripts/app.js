220051036953647dc3f87bab596a5ece8a3f1f36
Gabriel Gonzaga <gabrielmgonzaga@gmail.com>
2017-03-11 15:40:03 -0800

    Configure paths with location provider #2

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,14 @@
-angular.module('lyra', [ui.router])
+(function() {
+  function config($stateProvider, $locationProvider) {
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      })
+  }
+
+  angular
+    .module('lyra', ['ui.router'])
+    .config(config)
+
+})()
