8721a96bea3be2544682b1a28e6773cd70228e8c
Gabriel Gonzaga <gabrielmgonzaga@gmail.com>
2017-04-15 20:19:30 -0700

    Add logic to seekBar directive #19

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -6,7 +6,20 @@
       restrict: 'E',
       scope: { },
       link: function(scope, element, attributes) {
+        scope.value = 0
+        scope.max = 100
 
+        const percentString = function() {
+          const value = scope.value
+          const max = scope.max
+          const percent = value / max * 100
+
+          return percent + "%"
+        }
+
+        scope.fillStyle = function() {
+          return { width: percentString() }
+        }
       }
     }
   }
